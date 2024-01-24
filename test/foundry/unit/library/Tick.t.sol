3c157993f95666d3592e27511bbfecd27b190f34
Sean <detectivekim1@gmail.com>
2024-01-24 15:07:26 +0900

    fix: reduce test size

--- test/foundry/unit/library/Tick.t.sol
+++ test/foundry/unit/library/Tick.t.sol
@@ -11,12 +11,20 @@ contract TickUnitTest is Test {
         tickWrapper = new TickWrapper();
     }
 
-    function testIndexToPrice() public {
+    function testMinTickToPrice() public {
         uint256 lastPrice = tickWrapper.toPrice(TickLibrary.MIN_TICK);
-        int24 tick;
-        uint256 price;
-        for (int24 index = TickLibrary.MIN_TICK + 1; index < TickLibrary.MAX_TICK; index++) {
-            price = tickWrapper.toPrice(index);
+
+        vm.expectRevert(abi.encodeWithSelector(TickLibrary.InvalidPrice.selector));
+        tickWrapper.fromPrice(lastPrice - 1);
+
+        int24 tick = tickWrapper.fromPrice(lastPrice);
+        assertEq(tick, TickLibrary.MIN_TICK, "MIN_PRICE");
+
+        tick = tickWrapper.fromPrice(lastPrice + 1);
+        assertEq(tick, TickLibrary.MIN_TICK, "MIN_PRICE");
+
+        for (int24 index = TickLibrary.MIN_TICK + 1; index < TickLibrary.MIN_TICK + 100000; index++) {
+            uint256 price = tickWrapper.toPrice(index);
 
             tick = tickWrapper.fromPrice(price - 1);
             assertEq(tick, index - 1, "LOWER_PRICE");
@@ -28,9 +36,86 @@ contract TickUnitTest is Test {
             assertEq(tick, index, "HIGHER_PRICE");
 
             uint256 spread = (price - lastPrice) * 1000000 / lastPrice;
-            assertGe(spread, 50);
-            assertLe(spread, 200);
+            assertGe(spread, 99);
+            assertLe(spread, 100);
             lastPrice = price;
         }
     }
+
+    function testMiddleTickToPrice() public {
+        uint256 lastPrice = tickWrapper.toPrice(-100001);
+
+        for (int24 index = -100000; index < 100000; index++) {
+            uint256 price = tickWrapper.toPrice(index);
+
+            int24 tick = tickWrapper.fromPrice(price - 1);
+            assertEq(tick, index - 1, "LOWER_PRICE");
+
+            tick = tickWrapper.fromPrice(price);
+            assertEq(tick, index, "EXACT_PRICE");
+
+            tick = tickWrapper.fromPrice(price + 1);
+            assertEq(tick, index, "HIGHER_PRICE");
+
+            uint256 spread = (price - lastPrice) * 1000000 / lastPrice;
+            assertGe(spread, 99);
+            assertLe(spread, 100);
+            lastPrice = price;
+        }
+    }
+
+    function testMaxTickToPrice() public {
+        uint256 lastPrice = tickWrapper.toPrice(TickLibrary.MAX_TICK);
+
+        vm.expectRevert(abi.encodeWithSelector(TickLibrary.InvalidPrice.selector));
+        tickWrapper.fromPrice(lastPrice + 1);
+
+        int24 tick = tickWrapper.fromPrice(lastPrice);
+        assertEq(tick, TickLibrary.MAX_TICK, "MAX_PRICE");
+
+        tick = tickWrapper.fromPrice(lastPrice - 1);
+        assertEq(tick, TickLibrary.MAX_TICK - 1, "MAX_PRICE");
+
+        for (int24 index = TickLibrary.MAX_TICK - 1; index < TickLibrary.MAX_TICK - 100000; index--) {
+            uint256 price = tickWrapper.toPrice(index);
+
+            tick = tickWrapper.fromPrice(price - 1);
+            assertEq(tick, index - 1, "LOWER_PRICE");
+
+            tick = tickWrapper.fromPrice(price);
+            assertEq(tick, index, "EXACT_PRICE");
+
+            tick = tickWrapper.fromPrice(price + 1);
+            assertEq(tick, index, "HIGHER_PRICE");
+
+            uint256 spread = (lastPrice - price) * 1000000 / lastPrice;
+            assertGe(spread, 99);
+            assertLe(spread, 100);
+            lastPrice = price;
+        }
+    }
+
+    //    // Have to check all ticks is validate.
+    //    function testIndexToPrice() public {
+    //        uint256 lastPrice = tickWrapper.toPrice(TickLibrary.MIN_TICK);
+    //        int24 tick;
+    //        uint256 price;
+    //        for (int24 index = TickLibrary.MIN_TICK + 1; index < TickLibrary.MAX_TICK; index++) {
+    //            price = tickWrapper.toPrice(index);
+    //
+    //            tick = tickWrapper.fromPrice(price - 1);
+    //            assertEq(tick, index - 1, "LOWER_PRICE");
+    //
+    //            tick = tickWrapper.fromPrice(price);
+    //            assertEq(tick, index, "EXACT_PRICE");
+    //
+    //            tick = tickWrapper.fromPrice(price + 1);
+    //            assertEq(tick, index, "HIGHER_PRICE");
+    //
+    //            uint256 spread = (price - lastPrice) * 1000000 / lastPrice;
+    //            assertGe(spread, 99);
+    //            assertLe(spread, 100);
+    //            lastPrice = price;
+    //        }
+    //    }
 }
