16f94a18b3560111fd5ac33aa02b6d3ec9219b18
Sean <detectivekim1@gmail.com>
2024-01-25 15:15:57 +0900

    feat: use `IERC20Permit`, `IERC721Permit`

--- contracts/controller/CloberController.sol
+++ contracts/controller/CloberController.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 import "../interfaces/ICloberController.sol";
 import "../interfaces/IPositionLocker.sol";
 import "../interfaces/IBookManager.sol";
 import "../libraries/OrderId.sol";
 
-import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
-
 contract CloberController is ICloberController, IPositionLocker {
     using TickLibrary for *;
     using OrderIdLibrary for OrderId;
+    using SafeERC20 for IERC20;
     using CurrencyLibrary for Currency;
 
     IBookManager private immutable _bookManager;
@@ -26,20 +29,6 @@ contract CloberController is ICloberController, IPositionLocker {
             revert Deadline();
         }
         _;
-    }
-
-    function _permitERC20(address token, ERC20PermitParams memory p) internal {
-        if (p.signature.deadline > 0) {
-            try IERC20Permit(token).permit(
-                msg.sender,
-                address(this),
-                p.permitAmount,
-                p.signature.deadline,
-                p.signature.v,
-                p.signature.r,
-                p.signature.s
-            ) {} catch {}
-        }
     }
 
     function positionLockAcquired(bytes memory data) external returns (bytes memory result) {
@@ -58,6 +47,52 @@ contract CloberController is ICloberController, IPositionLocker {
         } else if (action == 2) {
             (SpendOrderParams[] memory paramsList) = abi.decode(data, (SpendOrderParams[]));
             _spend(paramsList);
+        }
+    }
+
+    function make(MakeOrderParams[] calldata paramsList, uint64 deadline)
+        external
+        payable
+        returns (OrderId[] memory ids)
+    {
+        bytes memory lockData = abi.encode(0, abi.encode(paramsList));
+        bytes memory result = _bookManager.lock(address(this), lockData);
+        (ids) = abi.decode(result, (OrderId[]));
+    }
+
+    function take(TakeOrderParams[] calldata paramsList, uint64 deadline) external payable checkDeadline(deadline) {
+        bytes memory lockData = abi.encode(1, abi.encode(paramsList));
+        _bookManager.lock(address(this), lockData);
+    }
+
+    function spend(SpendOrderParams[] calldata paramsList, uint64 deadline) external payable checkDeadline(deadline) {
+        bytes memory lockData = abi.encode(2, abi.encode(paramsList));
+        _bookManager.lock(address(this), lockData);
+    }
+
+    function claim(ClaimOrderParams[] calldata paramsList, uint64 deadline) external checkDeadline(deadline) {
+        // claim bounty
+        uint256 length = paramsList.length;
+        for (uint256 i = 0; i < length; i++) {
+            // Todo consider try catch
+            ClaimOrderParams memory params = paramsList[i];
+            _bookManager.claim(params.id, params.hookData);
+        }
+    }
+
+    function cancel(CancelOrderParams[] calldata paramsList, uint64 deadline) external checkDeadline(deadline) {
+        // claim bounty
+        uint256 length = paramsList.length;
+        for (uint256 i = 0; i < length; i++) {
+            // Todo consider try catch
+            CancelOrderParams memory params = paramsList[i];
+            (BookId bookId,,) = params.id.decode();
+            _permitERC721(OrderId.unwrap(params.id), params.permitParams);
+            _bookManager.cancel(
+                // Todo use safe toUint64
+                IBookManager.CancelParams({id: params.id, to: uint64(params.to / _bookManager.getBookKey(bookId).unit)}),
+                params.hookData
+            );
         }
     }
 
@@ -103,7 +138,6 @@ contract CloberController is ICloberController, IPositionLocker {
                 );
                 if (quoteAmount == 0) break;
                 _bookManager.withdraw(key.quote, address(this), quoteAmount);
-                key.base.transfer(address(_bookManager), baseAmount);
 
                 unchecked {
                     // Todo check underflow, overflow
@@ -112,6 +146,9 @@ contract CloberController is ICloberController, IPositionLocker {
                 }
             }
             if (params.maxBaseAmount < spendBaseAmount) revert ControllerSlippage();
+
+            _permitERC20(Currency.unwrap(key.base), params.permitParams);
+            IERC20(Currency.unwrap(key.base)).safeTransferFrom(msg.sender, address(_bookManager), spendBaseAmount);
             _bookManager.settle(key.base);
         }
     }
@@ -134,7 +171,6 @@ contract CloberController is ICloberController, IPositionLocker {
                 );
                 if (quoteAmount == 0) break;
                 _bookManager.withdraw(key.quote, address(this), quoteAmount);
-                key.base.transfer(address(_bookManager), baseAmount);
 
                 unchecked {
                     // Todo check underflow, overflow
@@ -143,60 +179,34 @@ contract CloberController is ICloberController, IPositionLocker {
                 }
             }
             if (takenQuoteAmount < params.minQuoteAmount) revert ControllerSlippage();
+
+            _permitERC20(Currency.unwrap(key.base), params.permitParams);
+            unchecked {
+                IERC20(Currency.unwrap(key.base)).safeTransferFrom(
+                    msg.sender, address(_bookManager), params.baseAmount - leftBaseAmount
+                );
+            }
             _bookManager.settle(key.base);
         }
     }
 
-    function make(MakeOrderParams[] calldata paramsList, uint64 deadline)
-        external
-        payable
-        returns (OrderId[] memory ids)
-    {
-        bytes memory lockData = abi.encode(0, abi.encode(paramsList));
-        bytes memory result = _bookManager.lock(address(this), lockData);
-        (ids) = abi.decode(result, (OrderId[]));
-    }
-
-    function take(TakeOrderParams[] calldata paramsList, uint64 deadline)
-        external
-        payable
-        checkDeadline(deadline)
-    {
-        bytes memory lockData = abi.encode(1, abi.encode(paramsList));
-        _bookManager.lock(address(this), lockData);
-    }
-
-    function spend(SpendOrderParams[] calldata paramsList, uint64 deadline)
-        external
-        payable
-        checkDeadline(deadline)
-    {
-        bytes memory lockData = abi.encode(2, abi.encode(paramsList));
-        _bookManager.lock(address(this), lockData);
-    }
-
-    function claim(ClaimOrderParams[] calldata paramsList, uint64 deadline) external checkDeadline(deadline) {
-        // claim bounty
-        uint256 length = paramsList.length;
-        for (uint256 i = 0; i < length; i++) {
-            // Todo consider try catch
-            ClaimOrderParams memory params = paramsList[i];
-            _bookManager.claim(params.id, params.hookData);
+    function _permitERC20(address token, ERC20PermitParams memory p) internal {
+        if (p.signature.deadline > 0) {
+            try IERC20Permit(token).permit(
+                msg.sender,
+                address(this),
+                p.permitAmount,
+                p.signature.deadline,
+                p.signature.v,
+                p.signature.r,
+                p.signature.s
+            ) {} catch {}
         }
     }
 
-    function cancel(CancelOrderParams[] calldata paramsList, uint64 deadline) external checkDeadline(deadline) {
-        // claim bounty
-        uint256 length = paramsList.length;
-        for (uint256 i = 0; i < length; i++) {
-            // Todo consider try catch
-            CancelOrderParams memory params = paramsList[i];
-            (BookId bookId,,) = params.id.decode();
-            _bookManager.cancel(
-                // Todo use safe toUint64
-                IBookManager.CancelParams({id: params.id, to: uint64(params.to / _bookManager.getBookKey(bookId).unit)}),
-                params.hookData
-            );
+    function _permitERC721(uint256 tokenId, PermitSignature memory p) internal {
+        if (p.deadline > 0) {
+            try IERC721Permit(address(_bookManager)).permit(msg.sender, tokenId, p.deadline, p.v, p.r, p.s) {} catch {}
         }
     }
 }
