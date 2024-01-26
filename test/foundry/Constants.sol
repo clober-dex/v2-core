b73b79c9e3ca1b7b96f7db0834371f586bfc37e8
Sean <detectivekim1@gmail.com>
2024-01-26 15:04:26 +0900

    test: `CloberControllerMakeOrderTest`

--- test/foundry/Constants.sol
+++ test/foundry/Constants.sol
@@ -4,4 +4,12 @@ pragma solidity ^0.8.20;
 
 library Constants {
     address internal constant DEFAULT_PROVIDER = address(0x9999);
+
+    address constant USER = address(0xabc);
+    uint64 public constant RAW_AMOUNT = 1873;
+    int24 public constant PRICE_TICK = 2848;
+    uint256 public constant PRICE = 18000000000000000;
+    uint256 public constant QUOTE_AMOUNT = 200 * 10 ** 18 + 123;
+    address public constant MAKER = address(0xbcd);
+    address public constant TAKER = address(0xcde);
 }
