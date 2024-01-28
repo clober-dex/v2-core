46b729de57f6bbb011f8a2c6933f90a80edc013e
Sean <detectivekim1@gmail.com>
2024-01-28 19:30:09 +0900

    test: `ControllerSpendOrderTest`

--- test/foundry/Constants.sol
+++ test/foundry/Constants.sol
@@ -12,6 +12,7 @@ library Constants {
     uint256 public constant QUOTE_AMOUNT1 = 200 * 10 ** 18 + 123;
     uint256 public constant QUOTE_AMOUNT2 = 152 * 10 ** 18 + 7347;
     uint256 public constant QUOTE_AMOUNT3 = 94 * 10 ** 18 + 461767;
+    uint256 public constant QUOTE_AMOUNT4 = 500 * 10 ** 18 + 75347;
     uint256 public constant BASE_AMOUNT1 = 12 * 10 ** 18 + 23432;
     address public constant MAKER1 = address(0xbcd1);
     address public constant MAKER2 = address(0xbcd2);
