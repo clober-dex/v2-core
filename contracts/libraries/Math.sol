7d707960675122e220497b6ba7300c773a98bca1
Sean <detectivekim1@gmail.com>
2024-04-05 18:05:01 +0900

    feat: use `Fixed96` for price

--- contracts/libraries/Math.sol
+++ contracts/libraries/Math.sol
@@ -14,28 +14,75 @@ library Math {
         }
     }
 
-    function log2(uint256 x) internal pure returns (int256) {
-        require(x > 0);
+    /// @dev Returns `ln(x)`, denominated in `WAD`.
+    /// Credit to Remco Bloemen under MIT license: https://2π.com/22/exp-ln
+    function lnWad(int256 x) external pure returns (int256 r) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // We want to convert `x` from `10**18` fixed point to `2**96` fixed point.
+            // We do this by multiplying by `2**96 / 10**18`. But since
+            // `ln(x * C) = ln(x) + ln(C)`, we can simply do nothing here
+            // and add `ln(2**96 / 10**18)` at the end.
 
-        uint8 msb = x.mostSignificantBit();
-
-        if (msb > 128) x >>= msb - 128;
-        else if (msb < 128) x <<= 128 - msb;
-
-        x &= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
-
-        int256 result = (int256(uint256(msb)) - 128) << 128; // Integer part of log_2
-
-        int256 bit = 0x80000000000000000000000000000000;
-        for (uint8 i = 0; i < 128 && x > 0; i++) {
-            x = (x << 1) + ((x * x + 0x80000000000000000000000000000000) >> 128);
-            if (x > 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) {
-                result |= bit;
-                x = (x >> 1) - 0x80000000000000000000000000000000;
+            // Compute `k = log2(x) - 96`, `r = 159 - k = 255 - log2(x) = 255 ^ log2(x)`.
+            r := shl(7, lt(0xffffffffffffffffffffffffffffffff, x))
+            r := or(r, shl(6, lt(0xffffffffffffffff, shr(r, x))))
+            r := or(r, shl(5, lt(0xffffffff, shr(r, x))))
+            r := or(r, shl(4, lt(0xffff, shr(r, x))))
+            r := or(r, shl(3, lt(0xff, shr(r, x))))
+            // We place the check here for more optimal stack operations.
+            if iszero(sgt(x, 0)) {
+                mstore(0x00, 0x1615e638) // `LnWadUndefined()`.
+                revert(0x1c, 0x04)
             }
-            bit >>= 1;
-        }
+            // forgefmt: disable-next-item
+            r := xor(r, byte(and(0x1f, shr(shr(r, x), 0x8421084210842108cc6318c6db6d54be)),
+                0xf8f9f9faf9fdfafbf9fdfcfdfafbfcfef9fafdfafcfcfbfefafafcfbffffffff))
 
-        return result;
+            // Reduce range of x to (1, 2) * 2**96
+            // ln(2^k * x) = k * ln(2) + ln(x)
+            x := shr(159, shl(r, x))
+
+            // Evaluate using a (8, 8)-term rational approximation.
+            // `p` is made monic, we will multiply by a scale factor later.
+            // forgefmt: disable-next-item
+            let p := sub( // This heavily nested expression is to avoid stack-too-deep for via-ir.
+                sar(96, mul(add(43456485725739037958740375743393,
+                    sar(96, mul(add(24828157081833163892658089445524,
+                        sar(96, mul(add(3273285459638523848632254066296,
+                            x), x))), x))), x)), 11111509109440967052023855526967)
+            p := sub(sar(96, mul(p, x)), 45023709667254063763336534515857)
+            p := sub(sar(96, mul(p, x)), 14706773417378608786704636184526)
+            p := sub(mul(p, x), shl(96, 795164235651350426258249787498))
+            // We leave `p` in `2**192` basis so we don't need to scale it back up for the division.
+
+            // `q` is monic by convention.
+            let q := add(5573035233440673466300451813936, x)
+            q := add(71694874799317883764090561454958, sar(96, mul(x, q)))
+            q := add(283447036172924575727196451306956, sar(96, mul(x, q)))
+            q := add(401686690394027663651624208769553, sar(96, mul(x, q)))
+            q := add(204048457590392012362485061816622, sar(96, mul(x, q)))
+            q := add(31853899698501571402653359427138, sar(96, mul(x, q)))
+            q := add(909429971244387300277376558375, sar(96, mul(x, q)))
+
+            // `p / q` is in the range `(0, 0.125) * 2**96`.
+
+            // Finalization, we need to:
+            // - Multiply by the scale factor `s = 5.549…`.
+            // - Add `ln(2**96 / 10**18)`.
+            // - Add `k * ln(2)`.
+            // - Multiply by `10**18 / 2**96 = 5**18 >> 78`.
+
+            // The q polynomial is known not to have zeros in the domain.
+            // No scaling required because p is already `2**96` too large.
+            p := sdiv(p, q)
+            // Multiply by the scaling factor: `s * 5**18 * 2**96`, base is now `5**18 * 2**192`.
+            p := mul(1677202110996718588342820967067443963516166, p)
+            // Add `ln(2) * k * 5**18 * 2**192`.
+            // forgefmt: disable-next-item
+            p := add(mul(16597577552685614221487285958193947469193820559219878177908093499208371, sub(159, r)), p)
+            // Base conversion: mul `2**96 / (5**18 * 2**192)`.
+            r := sdiv(p, 302231454903657293676544000000000000000000)
+        }
     }
 }
--- contracts/libraries/Tick.sol
+++ contracts/libraries/Tick.sol
@@ -7,7 +7,7 @@ import {Math} from "./Math.sol";
 type Tick is int24;
 
 library TickLibrary {
-    using Math for uint256;
+    using Math for *;
     using TickLibrary for Tick;
 
     error InvalidTick();
@@ -17,28 +17,29 @@ library TickLibrary {
     int24 internal constant MAX_TICK = 2 ** 19 - 1;
     int24 internal constant MIN_TICK = -MAX_TICK;
 
-    uint256 internal constant MIN_PRICE = 5800731190957938;
-    uint256 internal constant MAX_PRICE = 19961636804996334433808922353085948875386438476189866322430503;
+    uint256 internal constant MIN_PRICE = 1350587;
+    uint256 internal constant MAX_PRICE = 4647684107270898330752324302845848816923571339324334;
 
-    uint256 private constant _R0 = 0xfff97272373d413259a46990580e2139; // 2^128 / r^(2^0)
-    uint256 private constant _R1 = 0xfff2e50f5f656932ef12357cf3c7fdcb;
-    uint256 private constant _R2 = 0xffe5caca7e10e4e61c3624eaa0941ccf;
-    uint256 private constant _R3 = 0xffcb9843d60f6159c9db58835c926643;
-    uint256 private constant _R4 = 0xff973b41fa98c081472e6896dfb254bf;
-    uint256 private constant _R5 = 0xff2ea16466c96a3843ec78b326b52860;
-    uint256 private constant _R6 = 0xfe5dee046a99a2a811c461f1969c3052;
-    uint256 private constant _R7 = 0xfcbe86c7900a88aedcffc83b479aa3a3;
-    uint256 private constant _R8 = 0xf987a7253ac413176f2b074cf7815e53;
-    uint256 private constant _R9 = 0xf3392b0822b70005940c7a398e4b70f2;
-    uint256 private constant _R10 = 0xe7159475a2c29b7443b29c7fa6e889d8;
-    uint256 private constant _R11 = 0xd097f3bdfd2022b8845ad8f792aa5825;
-    uint256 private constant _R12 = 0xa9f746462d870fdf8a65dc1f90e061e4;
-    uint256 private constant _R13 = 0x70d869a156d2a1b890bb3df62baf32f6;
-    uint256 private constant _R14 = 0x31be135f97d08fd981231505542fcfa5;
-    uint256 private constant _R15 = 0x9aa508b5b7a84e1c677de54f3e99bc8;
-    uint256 private constant _R16 = 0x5d6af8dedb81196699c329225ee604;
-    uint256 private constant _R17 = 0x2216e584f5fa1ea926041bedfe97;
-    uint256 private constant _R18 = 0x48a170391f7dc42444e8fa2;
+    uint256 private constant _R0 = 0xfff97272373d413259a46990;
+    uint256 private constant _R1 = 0xfff2e50f5f656932ef12357c;
+    uint256 private constant _R2 = 0xffe5caca7e10e4e61c3624ea;
+    uint256 private constant _R3 = 0xffcb9843d60f6159c9db5883;
+    uint256 private constant _R4 = 0xff973b41fa98c081472e6896;
+    uint256 private constant _R5 = 0xff2ea16466c96a3843ec78b3;
+    uint256 private constant _R6 = 0xfe5dee046a99a2a811c461f1;
+    uint256 private constant _R7 = 0xfcbe86c7900a88aedcffc83b;
+    uint256 private constant _R8 = 0xf987a7253ac413176f2b074c;
+    uint256 private constant _R9 = 0xf3392b0822b70005940c7a39;
+    uint256 private constant _R10 = 0xe7159475a2c29b7443b29c7f;
+    uint256 private constant _R11 = 0xd097f3bdfd2022b8845ad8f7;
+    uint256 private constant _R12 = 0xa9f746462d870fdf8a65dc1f;
+    uint256 private constant _R13 = 0x70d869a156d2a1b890bb3df6;
+    uint256 private constant _R14 = 0x31be135f97d08fd981231505;
+    uint256 private constant _R15 = 0x9aa508b5b7a84e1c677de54;
+    uint256 private constant _R16 = 0x5d6af8dedb81196699c329;
+    uint256 private constant _R17 = 0x2216e584f5fa1ea92604;
+    uint256 private constant _R18 = 0x48a170391f7dc42;
+    uint256 private constant _R19 = 0x149b34;
 
     function validateTick(Tick tick) internal pure {
         if (Tick.unwrap(tick) > MAX_TICK || Tick.unwrap(tick) < MIN_TICK) revert InvalidTick();
@@ -49,18 +50,12 @@ library TickLibrary {
         _;
     }
 
-    function fromPrice(uint256 price) internal pure validatePrice(price) returns (Tick) {
-        int256 log = price.log2();
-        int256 tick = log / 49089913871092318234424474366155889;
-        int256 tickLow = (
-            log - int256(uint256((price >> 128 == 0) ? 49089913871092318234424474366155887 : 84124744249948177485425))
-        ) / 49089913871092318234424474366155889;
-
-        if (tick == tickLow) return Tick.wrap(int24(tick));
-
-        if (toPrice(Tick.wrap(int24(tick))) <= price) return Tick.wrap(int24(tick));
-
-        return Tick.wrap(int24(tickLow));
+    function fromPrice(uint256 price) internal pure validatePrice(price) returns (Tick tick) {
+        unchecked {
+            int24 tick = int24((int256(price).lnWad() * 42951820407860) / 2 ** 128);
+            if (toPrice(Tick.wrap(tick)) > price) return Tick.wrap(tick - 1);
+            return Tick.wrap(tick);
+        }
     }
 
     function toPrice(Tick tick) internal pure returns (uint256 price) {
@@ -70,27 +65,27 @@ library TickLibrary {
 
         unchecked {
             if (absTick & 0x1 != 0) price = _R0;
-            else price = 1 << 128;
-            if (absTick & 0x2 != 0) price = (price * _R1) >> 128;
-            if (absTick & 0x4 != 0) price = (price * _R2) >> 128;
-            if (absTick & 0x8 != 0) price = (price * _R3) >> 128;
-            if (absTick & 0x10 != 0) price = (price * _R4) >> 128;
-            if (absTick & 0x20 != 0) price = (price * _R5) >> 128;
-            if (absTick & 0x40 != 0) price = (price * _R6) >> 128;
-            if (absTick & 0x80 != 0) price = (price * _R7) >> 128;
-            if (absTick & 0x100 != 0) price = (price * _R8) >> 128;
-            if (absTick & 0x200 != 0) price = (price * _R9) >> 128;
-            if (absTick & 0x400 != 0) price = (price * _R10) >> 128;
-            if (absTick & 0x800 != 0) price = (price * _R11) >> 128;
-            if (absTick & 0x1000 != 0) price = (price * _R12) >> 128;
-            if (absTick & 0x2000 != 0) price = (price * _R13) >> 128;
-            if (absTick & 0x4000 != 0) price = (price * _R14) >> 128;
-            if (absTick & 0x8000 != 0) price = (price * _R15) >> 128;
-            if (absTick & 0x10000 != 0) price = (price * _R16) >> 128;
-            if (absTick & 0x20000 != 0) price = (price * _R17) >> 128;
-            if (absTick & 0x40000 != 0) price = (price * _R18) >> 128;
+            else price = 1 << 96;
+            if (absTick & 0x2 != 0) price = (price * _R1) >> 96;
+            if (absTick & 0x4 != 0) price = (price * _R2) >> 96;
+            if (absTick & 0x8 != 0) price = (price * _R3) >> 96;
+            if (absTick & 0x10 != 0) price = (price * _R4) >> 96;
+            if (absTick & 0x20 != 0) price = (price * _R5) >> 96;
+            if (absTick & 0x40 != 0) price = (price * _R6) >> 96;
+            if (absTick & 0x80 != 0) price = (price * _R7) >> 96;
+            if (absTick & 0x100 != 0) price = (price * _R8) >> 96;
+            if (absTick & 0x200 != 0) price = (price * _R9) >> 96;
+            if (absTick & 0x400 != 0) price = (price * _R10) >> 96;
+            if (absTick & 0x800 != 0) price = (price * _R11) >> 96;
+            if (absTick & 0x1000 != 0) price = (price * _R12) >> 96;
+            if (absTick & 0x2000 != 0) price = (price * _R13) >> 96;
+            if (absTick & 0x4000 != 0) price = (price * _R14) >> 96;
+            if (absTick & 0x8000 != 0) price = (price * _R15) >> 96;
+            if (absTick & 0x10000 != 0) price = (price * _R16) >> 96;
+            if (absTick & 0x20000 != 0) price = (price * _R17) >> 96;
+            if (absTick & 0x40000 != 0) price = (price * _R18) >> 96;
         }
-        if (tickValue > 0) price = type(uint256).max / price;
+        if (tickValue > 0) price = 0x1000000000000000000000000000000000000000000000000 / price;
     }
 
     function gt(Tick a, Tick b) internal pure returns (bool) {
