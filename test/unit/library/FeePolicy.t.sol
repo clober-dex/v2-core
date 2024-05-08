50e38324478ed80d9faedfa3ac0be32feb06826a
JChoy <jchoyeclipse@gmail.com>
2024-05-08 16:12:55 +0900

    chore: remove warnings

--- test/unit/library/FeePolicy.t.sol
+++ test/unit/library/FeePolicy.t.sol
@@ -9,14 +9,14 @@ import "../../../src/libraries/FeePolicy.sol";
 contract FeePolicyTest is Test {
     using FeePolicyLibrary for FeePolicy;
 
-    function testEncode(bool usesQuote, int24 rate) public {
+    function testEncode(bool usesQuote, int24 rate) public pure {
         vm.assume(rate <= FeePolicyLibrary.MAX_FEE_RATE && rate >= FeePolicyLibrary.MIN_FEE_RATE);
         FeePolicy feePolicy = FeePolicyLibrary.encode(usesQuote, rate);
         assertEq(feePolicy.usesQuote(), usesQuote);
         assertEq(feePolicy.rate(), rate);
     }
 
-    function testCalculateFee() public {
+    function testCalculateFee() public pure {
         _testCalculateFee(1000, 1000000, 1000, false);
         _testCalculateFee(-1000, 1000000, -1000, false);
         // zero value tests
@@ -29,7 +29,7 @@ contract FeePolicyTest is Test {
         _testCalculateFee(-1500, 1000, -2, true);
     }
 
-    function _testCalculateFee(int24 rate, uint256 amount, int256 fee, bool reverse) private {
+    function _testCalculateFee(int24 rate, uint256 amount, int256 fee, bool reverse) private pure {
         FeePolicy feePolicy = FeePolicyLibrary.encode(true, rate);
         int256 actualFee = feePolicy.calculateFee(amount, reverse);
         assertEq(actualFee, fee);
