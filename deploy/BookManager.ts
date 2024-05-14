5b7b6f8f71029fac3b2c89530e9de452f27145bb
JChoy <jchoyeclipse@gmail.com>
2024-05-14 21:49:06 +0900

    fix: fix BookManager deploy script

--- deploy/BookManager.ts
+++ deploy/BookManager.ts
@@ -18,25 +18,32 @@ const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnviro
     return
   }
 
-  let args: any[] = []
+  let owner = ''
+  let defaultProvider = ''
   if (chain.testnet || isDevelopmentNetwork(chain.id)) {
-    args = [deployer, deployer, 'baseURI', 'contractURI', 'Clober Orderbook Maker Order', 'CLOB-ORDER']
+    owner = defaultProvider = deployer
   } else if (chain.id === base.id) {
-    args = [
-      '0xfb976Bae0b3Ef71843F1c6c63da7Df2e44B3836d', // Safe
-      '0xfc5899d93df81ca11583bee03865b7b13ce093a7', // Treasury
-      `https://clober.io/api/nft/chains/${chain.id}/orders/`,
-      `https://clober.io/api/contract/chains/${chain.id}`,
-      'Clober Orderbook Maker Order',
-      'CLOB-ORDER',
-    ]
+    owner = '0xfb976Bae0b3Ef71843F1c6c63da7Df2e44B3836d' // Safe
+    defaultProvider = '0xfc5899d93df81ca11583bee03865b7b13ce093a7' // Treasury
   } else {
     throw new Error('Unknown chain')
   }
 
-  await deployWithVerify(hre, 'BookManager', args, {
-    Book: bookLibraryAddress,
-  })
+  await deployWithVerify(
+    hre,
+    'BookManager',
+    [
+      owner,
+      defaultProvider,
+      `https://clober.io/api/nft/chains/${chain.id}/orders/`,
+      `https://clober.io/api/contract/chains/${chain.id}`,
+      'Clober Orderbook Maker Order',
+      'CLOB-ORDER',
+    ],
+    {
+      Book: bookLibraryAddress,
+    },
+  )
 }
 
 deployFunction.tags = ['BookManager']
