a58ab31167c9943667b945df9b9c079636580ad1
Sean <detectivekim1@gmail.com>
2024-09-06 14:33:22 +0900

    chore: lint

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -86,11 +86,11 @@ const config: HardhatConfig = {
   zksolc: {
     version: 'latest', // Uses latest available in https://github.com/matter-labs/zksolc-bin/
     settings: {
-        libraries: {
-              "src/libraries/Book.sol": {
-                "Book": "0xAc742Cf41d12fA3835f2c658897D5D64a02eCEF8"
-              }
-            }
+      libraries: {
+        'src/libraries/Book.sol': {
+          Book: '0xAc742Cf41d12fA3835f2c658897D5D64a02eCEF8',
+        },
+      },
     },
   },
   solidity: {
